5a71dacf587be82720e22f2e88aa5e981d1ce0d7
rslouis66 <rslouis66@gmail.com>
2025-07-24 14:59:38 -0500

    Show SignalCard in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SignalCard from './SignalCard';
 
 function App() {
   return (
     <div className="App">
       <h1>SignalGlide</h1>
       <p>Welcome to your trading signals dashboard!</p>
+      <SignalCard />
     </div>
   );
 }
